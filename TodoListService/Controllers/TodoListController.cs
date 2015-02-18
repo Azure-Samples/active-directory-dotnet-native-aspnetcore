1d5389ed882749f97c975182b1b6012da8aa398f
dstrockis <dastrock@microsoft.com>
2015-02-18 08:52:04 -0800

    Initial Commit

---
+++ TodoListService/Controllers/TodoListController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using System.Security.Claims;
+using TodoListService.Models;
+using System.Collections.Concurrent;
+
+namespace TodoListService.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class TodoListController : Controller
+    {
+        static ConcurrentBag<TodoItem> todoStore = new ConcurrentBag<TodoItem>();
+
+        // GET: api/todolist
+        [HttpGet]
+        public IEnumerable<TodoItem> Get()
+        {
+            string owner = Context.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return todoStore.Where(t => t.Owner == owner).ToList();
+        }
+    
+        // POST api/todolist
+        [HttpPost]
+        public void Post(string Title)
+        {
+            string owner = Context.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            todoStore.Add(new TodoItem { Owner = owner, Title = Title });
+        }
+    }
+}
